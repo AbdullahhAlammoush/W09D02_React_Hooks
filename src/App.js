5cf4a44a9b518edde7c33c5a73a143341d8e8932
AbdullahhAlammoush <Abdullahh.Alammoush@gmail,com>
2022-01-04 01:21:34 +0200

    1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React,{useState} from "react";
+import "./App.css";
+
+export default function App() {
+  const [posts,setPosts]=useState("")
+  return (
+    <div className="App">
+      <div>Blog app</div>
+      <p>APP</p>
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// const React=require('react')
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// ReactDOM.render(
+//   <App></App>,
+//   document.querySelector('#root')
+//  );
+ 
