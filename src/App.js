1a6d50cf5facbadbf575210c473e3d4006ab5ebf
AbdullahhAlammoush <Abdullahh.Alammoush@gmail,com>
2022-01-04 02:51:38 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-export default function App() {
-  const [posts,setPosts]=useState("")
+// export default function App() {
+const App = () => {
+  const [posts, setPosts] = useState(
+    { userId: 8, id: 800, title: "YOU", body: "broken" },
+    { userId: 6, id: 600, title: "life", body: "Wasted" }
+  );
+  const show = () => {
+    console.log("results");
+
+    setPosts(posts[0].userId);
+  };
+
   return (
     <div className="App">
       <div>Blog app</div>
       <p>APP</p>
+      {/* <p>{show()}</p> */}
+      <button onClick={show}>any</button>
     </div>
   );
-}
+};
+export default App;
+
+
+// q5
